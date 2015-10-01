54c79d5a055a012e7b62b6c0a10b03228ba54e7d
Balázs Bakai <bakaibalazs@gmail.com>
2015-10-01 10:39:53 +0200

    perform method

--- PipelineNumberUpdater/src/main/java/hu/bakaibalazs/jenkins/PipelineNumberUpdater/PipelineNumberBuilder.java
+++ PipelineNumberUpdater/src/main/java/hu/bakaibalazs/jenkins/PipelineNumberUpdater/PipelineNumberBuilder.java
@@ -11,90 +11,89 @@ import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import hudson.util.ArgumentListBuilder;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.Properties;
-import java.util.Scanner;
 
 import org.kohsuke.stapler.DataBoundConstructor;
 
 public class PipelineNumberBuilder extends Builder {
 
-	private static final String propertyFileName = "pipeline-version.properties";
+	private static final String LATEST_GIT_TAG_COMMAND="git describe --abbrev=0";
+	private static final String PROPERTY_FILE_NAME = "pipeline-version.properties";
+	
 	private static final Properties prop = new Properties();
+	private static PrintStream logger=null;
 
 	@DataBoundConstructor
 	public PipelineNumberBuilder() {
 	}
 
+	
+	/**
+	 * Save the latest git tag and the pipeline version into the property file
+	 * When a new git tag comes this plugin initialize the pipeline version to 1
+	 * 
+	 */
 	@Override
-	public boolean perform(AbstractBuild build, Launcher launcher,
-			BuildListener listener) throws IOException {
-		listener.getLogger().println("---perform---");
+	public boolean perform(AbstractBuild build, Launcher launcher,BuildListener listener) throws IOException {
+		logger=listener.getLogger();
+		logger.println("perform method invoked");
 
-		String propertyFilePath = build.getWorkspace() + File.separator
-				+ propertyFileName;
+		String propertyFilePath = build.getWorkspace() + File.separator+ PROPERTY_FILE_NAME;
+		String savedPipelineVersion = readFromPropertyFile(propertyFilePath,PropertyName.PIPELINE_NUMBER.name());
+		String savedGitTag = readFromPropertyFile(propertyFilePath,PropertyName.LATEST_GIT_TAG.name());
+		String currentGitTag = executeCommand(build, launcher, listener,LATEST_GIT_TAG_COMMAND);
 
-		String currentPipelineVersion = readFromPropertyFile(propertyFilePath,"PIPELINE_NUMBER");
-		String latestGitTag = readFromPropertyFile(propertyFilePath,"LATEST_GIT_TAG");
-		String currentGitTag = executeCommand(build,launcher,listener,"git describe --abbrev=0");
-
+		if(currentGitTag==null){
+			logger.println("YOU HAVE TO CREATE A GIT TAG BEFORE YOU USE THIS PLUGIN");
+			return false;
+		}
 		
-					
-		listener.getLogger().println("currentPipelineVersion: " + currentPipelineVersion);
-		listener.getLogger().println("latestGitTag: " + latestGitTag);
-		listener.getLogger().println("currentGitTag: " + currentGitTag);
+		
+		logger.println("savedPipelineVersion: " + savedPipelineVersion);
+		logger.println("savedGitTag: " + savedGitTag);
+		logger.println("currentGitTag: " + currentGitTag);
 
-		if (currentPipelineVersion==null) {
-			writeToPropertyFile(propertyFilePath, "PIPELINE_NUMBER", "1");
+		if (savedPipelineVersion == null) {
+			writeIntoPropertyFile(propertyFilePath, PropertyName.PIPELINE_NUMBER.name(), "1");
 		} else {
-			writeToPropertyFile(propertyFilePath, "PIPELINE_NUMBER",
-					String.valueOf(Integer.valueOf(currentPipelineVersion) + 1));
+			writeIntoPropertyFile(propertyFilePath, PropertyName.PIPELINE_NUMBER.name(),String.valueOf(Integer.valueOf(savedPipelineVersion) + 1));
 		}
 
-		if (latestGitTag==null || !latestGitTag.equals(currentGitTag)) {
-			writeToPropertyFile(propertyFilePath, "LATEST_GIT_TAG",currentGitTag);
+		if (savedGitTag == null || !savedGitTag.equals(currentGitTag)) {
+			writeIntoPropertyFile(propertyFilePath, PropertyName.LATEST_GIT_TAG.name(),currentGitTag);
 		}
 
-		if (latestGitTag!=null && currentGitTag!=null && !latestGitTag.equals(currentGitTag)) {
-			writeToPropertyFile(propertyFilePath, "PIPELINE_NUMBER", "1");
+		if (savedGitTag != null && !savedGitTag.equals(currentGitTag)) {
+			writeIntoPropertyFile(propertyFilePath, PropertyName.PIPELINE_NUMBER.name(), "1");
 		}
 
 		return true;
 	}
 
-	private String executeCommand(AbstractBuild build, Launcher launcher,BuildListener listener,String command) {
-	
-		OutputStream out=new ByteArrayOutputStream();			
-		ArgumentListBuilder cmd = new ArgumentListBuilder();
-		cmd.addTokenized(command);		
+	private String executeCommand(AbstractBuild build, Launcher launcher,BuildListener listener, String command) {
+
+		OutputStream out = new ByteArrayOutputStream();
+		ArgumentListBuilder cmd = new ArgumentListBuilder();		
 		ProcStarter ps = launcher.new ProcStarter();
+		cmd.addTokenized(command);
 		ps = ps.cmds(cmd).stdout(out);
 		try {
 			ps = ps.pwd(build.getWorkspace()).envs(build.getEnvironment(listener));
 			Proc proc = launcher.launch(ps);
-			proc.join();			
-			return new String(((ByteArrayOutputStream)out).toByteArray(), "UTF-8").replace("\n", "").replace("\r", "");			
-		}catch(Exception e){
-			stackTraceToString(e);
+			proc.join();
+			return new String(((ByteArrayOutputStream) out).toByteArray(),"UTF-8").replace("\n", "").replace("\r", "");
+		} catch (Exception e) {
+			logger.println(stackTraceToString(e));
 			return "";
 		}
-	}
-
-	private String stackTraceToString(Throwable e) {
-		StringBuilder sb = new StringBuilder();
-		for (StackTraceElement element : e.getStackTrace()) {
-			sb.append(element.toString());
-			sb.append("\n");
-		}
-		return sb.toString();
 	}
 
 	private String readFromPropertyFile(String propertyFilePath, String name) {
@@ -103,21 +102,20 @@ public class PipelineNumberBuilder extends Builder {
 			prop.load(new FileInputStream(propertyFilePath));
 			return prop.getProperty(name);
 		} catch (IOException ex) {
-			ex.printStackTrace();
+			logger.println(stackTraceToString(ex));
 			return null;
 		} finally {
 			if (input != null) {
 				try {
 					input.close();
 				} catch (IOException e) {
-					e.printStackTrace();
+					logger.println(stackTraceToString(e));
 				}
 			}
 		}
 	}
 
-	private void writeToPropertyFile(String propertyFilePath, String name,
-			String value) {
+	private void writeIntoPropertyFile(String propertyFilePath, String name,String value) {
 		OutputStream output = null;
 		try {
 			output = new FileOutputStream(propertyFilePath);
@@ -125,18 +123,26 @@ public class PipelineNumberBuilder extends Builder {
 			prop.setProperty(name, value);
 			prop.store(output, null);
 		} catch (IOException io) {
-			io.printStackTrace();
+			logger.println(stackTraceToString(io));
 		} finally {
 			if (output != null) {
 				try {
 					output.close();
 				} catch (IOException e) {
-					e.printStackTrace();
+					logger.println(stackTraceToString(e));
 				}
 			}
 		}
-
 	}
+	
+	private String stackTraceToString(Throwable e) {
+		StringBuilder sb = new StringBuilder();
+		for (StackTraceElement element : e.getStackTrace()) {
+			sb.append(element.toString());
+			sb.append("\n");
+		}
+		return sb.toString();
+	}	
 
 	@Override
 	public DescriptorImpl getDescriptor() {
@@ -144,19 +150,14 @@ public class PipelineNumberBuilder extends Builder {
 	}
 
 	@Extension
-	public static final class DescriptorImpl extends
-			BuildStepDescriptor<Builder> {
+	public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
 
 		public boolean isApplicable(Class<? extends AbstractProject> aClass) {
-			return true; // Indicates that this builder can be used with all
-							// kinds of project types
+			return true;
 		}
 
 		public String getDisplayName() {
-			return "Pipeline Number Updater"; // This human readable name is
-												// used in the configuration
-												// screen.
+			return "Pipeline Number Updater";
 		}
-
 	}
 }
---
+++ PipelineNumberUpdater/src/main/java/hu/bakaibalazs/jenkins/PipelineNumberUpdater/PropertyName.java
@@ -0,0 +1,5 @@
+package hu.bakaibalazs.jenkins.PipelineNumberUpdater;
+
+public enum PropertyName {
+	PIPELINE_NUMBER,LATEST_GIT_TAG;
+}
